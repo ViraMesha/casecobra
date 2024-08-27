d0f5453c78b52eb3a945338624b1db76ad6bbdec
ViraMesha <wira.mescha@gmail.com>
2024-08-27 22:55:42 +0300

    feat: Write backend logic for saving img config data to db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,9 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
 
+// Create an instance of Uploadthing for file handling
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
@@ -13,13 +16,45 @@ export const ourFileRouter = {
         configId: z.string().optional(),
       })
     )
+    // Middleware to process the input before the file upload
     .middleware(async ({ input }) => {
+      // Pass the input to the next step
       return { input };
     })
+    // Handle actions after the file upload is complete
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input;
-      return { configId };
-    }),
-} satisfies FileRouter;
+      const { configId } = metadata.input; // Extract 'configId' from metadata
 
+      // Fetch the uploaded file's URL
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            height: height || 500,
+            width: width || 500,
+            imageUrl: file.url,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        return { configId: updatedConfiguration.id };
+      }
+    }),
+} satisfies FileRouter; // Ensure ourFileRouter conforms to the FileRouter type
+
+// Export the type of ourFileRouter for use elsewhere in the application
 export type OurFileRouter = typeof ourFileRouter;
