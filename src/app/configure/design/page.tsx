cd2566a4d36bb40c0e6dbba5c6ff18e6088c7703
ViraMesha <wira.mescha@gmail.com>
2024-08-28 22:47:18 +0300

    feat: Implement design page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,24 @@
-const Page = async () => {
-  return <div>Page</div>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+const Page = async ({ searchParams }: SearchParamProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return <div>{id}</div>;
 };
 
 export default Page;
